d4a25cef243c928a2ffc6963b4045e1d6862dfbc
John Wu <john.wu@bcbstx.com>
2015-06-18 14:49:40 -0500

    Hook up with mongoDB

--- RESTfulWS/src/main/java/com/myapp/dao/UserDaoImpl.java
+++ RESTfulWS/src/main/java/com/myapp/dao/UserDaoImpl.java
@@ -1,14 +1,29 @@
 package com.myapp.dao;
 
+import javax.inject.Inject;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
 import com.myapp.domain.User;
 
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
+  @Inject
+  MongoOperations mongoOperator;
+
   public User findUser(String userName){
-    return new User();
+
+  Query searchUserQuery = new Query(Criteria.where("userName").is(userName));
+
+  User user = mongoOperator.findOne(searchUserQuery, User.class);
+  System.out.println("2. find - savedUser : " + user);
+    return user;
   }
 
 }
--- RESTfulWS/src/main/java/com/myapp/domain/User.java
+++ RESTfulWS/src/main/java/com/myapp/domain/User.java
@@ -1,9 +1,25 @@
 package com.myapp.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
 public class User {
+
+  @Id
+  private String id;
 
   private String userName;
   private String passWord;
+
+
+ public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 
  public String getUserName() {
     return userName;
